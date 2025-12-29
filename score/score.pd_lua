b9ff89c0d6833bc735266a9015198c2c37890e1d
Artur J. Dobija <arturdobija@gmail.com>
2025-12-29 19:12:00 +0100

    Designed Event object.

--- score/score.pd_lua
+++ score/score.pd_lua
@@ -1,21 +1,60 @@
 local pd <const> = pd or require('./utilities/pd_stub')
 local o <const>  = pd.Class:new():register("score") ---@type Score
 
--- local Track <const> = {
+---Prepare a metatable to be implemented as a "class."
+---@generic I, J
+---@param name `I`
+---@param meta { [string]: any }
+---@param parent? Metatable<J>
+---@return Metatable<I>
+local function class(name, meta, parent)
+    local c <const> = {
+        __type = name,
+        table.unpack(meta)
+    }
+    c.__index = parent or c
+    return c
+end
 
--- }
+---@type Metatable<Event, fun(name?:string): Event>
+local Event <const> = class("Event", {})
+Event.new = function(name)
+    local event <const> = setmetatable({}, Event) ---@type Event
+    event.id = os.time()
+    event.name = name
+    event.duration = nil
+    event.props = {}
+    event.players = {}
+    return event
+end
 
+---comment
+---@return string
+function Event:tostring()
+    local tab = '        '
+    local id <const> = '(' .. self.id .. ')'
+    local s = "Event "
+        .. (self.name or '') .. ' ' .. id .. ' [' .. tostring(self) .. ']:\n'
+        .. tab .. "* duration: " .. tostring(self.duration)
+    return s
+end
 
+function o:get_track(track_name)
+    local track <const> = self.tracks[track_name]
+    if not track then
+        local track_new <const> = Event.new(track_name)
+        self.tracks[track_name] = track_new
+        return track_new
+    end
+    return track
+end
 
--- function o:get_track(track_name)
---     local track <const> = self.tracks[track_name]
---     if not track then
---         local track_new <const> = Track.new(track_name)
---         self.tracks[track_name] = track_new
---         return track_new
---     end
---     return track
--- end
+function o:get_event(event_label)
+    if type(event_label) == "number" then
+        pd.post("Unnamed Event " .. event_label .. " cannot be found (not implemented).")
+    end
+    return self.tracks[event_label]
+end
 
 --[[
 	    pd-lua.
@@ -45,7 +84,17 @@ function o:in_1_record(atoms)
     local track_name <const> = atoms[1]
     local toggle <const> = atoms[2]
 
-    -- local track <const> = self:get_track(track_name)
+    local track <const> = self:get_track(track_name)
+end
+
+function o:in_1_info(atoms)
+    local event_label <const> = atoms[1]
+    local event <const> = self:get_event(event_label)
+    if event then
+        pd.post('[score] ' .. event:tostring())
+    else
+        pd.post("[score] Event " .. event_label .. " does not exist.")
+    end
 end
 
 return o
--- score/test_score.types.lua
+++ score/test_score.types.lua
@@ -1,28 +1,35 @@
----@alias Miliseconds number
----@alias Date number
----@alias Speed number # The speed of an imaginary "tape". 0 is still, 1 is normal, -1 is backward and 2 is twice as fast.
----@alias TrackName string # Name of a Track.
+--[[    Helpers.    ]]
+---@class (exact) Metatable<I, Ctor>: { __type: string, __index: Metatable<I>, new?: Ctor, [any]: any }
 
----@class EventProps
-
----@class (exact) Player
----@field public event Event
----@field public offset Miliseconds
----@field public speed Speed
----@field public repeats number
-
----@class (exact) Event
----@field public born Date
----@field public duration Miliseconds
----@field public players Player
----@field public props EventProps
-
----@class (exact) Track: Event
----@field public name? TrackName
----@field public new Track
-
+--[[    Main object.    ]]
 ---@class (exact) Score: PDObject
----@field public tracks { [TrackName]: Track }
----@field public get_track fun(self: Score, track_name: TrackName): Track # Returns the track named track_name. If not such track exists yet, makes a new one.
+---@field public tracks { [EventName]: Event }
+---@field public get_track fun(self: Score, track_name: EventName): Event # Returns the track named track_name. If not such track exists yet, makes a new one.
+---@field public get_event fun(self: Score, event_label: EventName|EventID): Event|nil # Returns the track named track_name. If not such track exists yet, makes a new one.
+---@field public in_1_info fun(self: Score, atoms: { [1]: EventID|EventName}): nil
 ---@field public in_1_reload fun(self: Score): nil
 ---@field public in_1_record fun(self: Score, atoms: { [1]: string, [2]: number }): nil
+
+
+--[[    Score types.    ]]
+---@alias Miliseconds number
+---@alias EventID number
+---@alias Speed number # The speed of an imaginary "tape". 0 is still, 1 is normal, -1 is backward and 2 is twice as fast.
+---@alias EventName string
+--[[    Event types.    ]]
+---@class EventProps
+
+---@class (exact) Event
+---@field public id EventID When event was created.
+---@field public duration Miliseconds How long does the event lasts.
+---@field public players Player[] Which players does this event control.
+---@field public props EventProps[] What properties act out when event is called.
+---@field public name EventName
+---@field public tostring fun(self:Event): string
+
+--[[    Player types.   ]]
+---@class (exact) Player
+---@field public event Event What event to play next.
+---@field public offset Miliseconds How long to wait for event to play
+---@field public speed Speed How fast event acts out (1: as is).
+---@field public repeats number How many times player repeats its event.
