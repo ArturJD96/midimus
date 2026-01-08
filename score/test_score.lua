e26a676bbc03285cd34ed48f386e86ac3f502513
Artur J. Dobija <arturdobija@gmail.com>
2026-01-08 19:39:40 +0100

    Added real delay to tests.

--- score/test_score.lua
+++ score/test_score.lua
@@ -10,12 +10,19 @@ end
 function record_dummy_track(o)
     local t = "dummy"
     o:in_1_record({ t, 1 })
+    os.execute('sleep 0.1')
     o:in_1_midi({ 1, 2, 3, 4 })
+    os.execute('sleep 0.1')
     o:in_1_midi({ 5, 6, 7, 8 })
+    os.execute('sleep 0.1')
     o:in_1_record({ t, 0 })
     local track = o.tracks[t]
     return track
 end
+
+--[[
+        Test begins here.
+]]
 
 TestRecord = {}
 
@@ -32,6 +39,7 @@ TestPlay = {}
 function TestPlay:test_works()
     init(o)
     local track = record_dummy_track(o)
+    o:in_1_play({ track.name, 1 })
     o:in_1_play({ track.name, 1 })
     lu.assertNotNil(o.players[track.name])
     o:in_1_play({ track.name, 0 })
